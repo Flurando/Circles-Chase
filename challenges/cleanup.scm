e3a8d88e69968d6bb9636c3400cfe06c66979766
R.W.Flurando <Flurando@users.noreply.github.com>
2025-06-24 19:49:29 +0800

    format

--- challenges/cleanup.scm
+++ challenges/cleanup.scm
@@ -1,5 +1,5 @@
 (define cleanup!
-	  (lambda ()
-	    (enemy-clean!)
-	    (set! player-health 100.0)
-	    (set! play-timer (make-agenda))))
+  (lambda ()
+    (enemy-clean!)
+    (set! player-health 100.0)
+    (set! play-timer (make-agenda))))
